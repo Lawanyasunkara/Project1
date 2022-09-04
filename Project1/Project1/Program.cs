97352a106abf8a8126ea761967dc6c373867cc42
Lawanyasunkara <111355566+Lawanyasunkara@users.noreply.github.com>
2022-09-04 12:17:52 +0530

    Add files via upload

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"F:\Data.txt");
+            System.Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
